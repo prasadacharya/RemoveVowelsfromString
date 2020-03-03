01e0c8db02cccad7285317b485ac74b3919c4305
prasadacharya <prasada@mail.usf.edu>
2020-03-03 13:18:40 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿
+
+using System;
+using System.Text;
+
+namespace RemoveVowelsfromString
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            string A = "aeiou";
+            string result = RemoveVowels(A);
+            Console.WriteLine(result);
+        }
+        private static string RemoveVowels(string S)
+        {
+            StringBuilder s = new StringBuilder();
+            for (int i = 0; i < S.Length; i++)
+            {
+                if (S[i] == 'a' || S[i] == 'e' || S[i] == 'i' || S[i] == 'o' || S[i] == 'u')
+                    continue;
+                else
+                    s.Append(S[i]);
+            }
+
+            return s.ToString();
+        }
+    }
+}
